00e09ea97df1bbbd4b9be1211dc0d8ddf5122a82
Darrius-W <you@example.com>
2023-01-03 16:08:47 -0600

    Added label to hold number when it inc or dec

--- plusMinusGUI.py
+++ plusMinusGUI.py
@@ -4,8 +4,11 @@ window = tk.Tk() # creates instance of tkinter class
 
 frame1 = tk.Frame(master=window, width=100, height=100, bg='blue') # frame for the whole application
 
-minus_btn = tk.Button(master=frame1, text='-', background='red')
-minus_btn.pack()
+minus_btn = tk.Button(master=frame1, text='-', bg='red')
+minus_btn.pack(side=tk.LEFT)
+
+display_lbl = tk.Label(master=frame1, text='"display num"', bg='green')
+display_lbl.pack(side=tk.LEFT)
 
 frame1.pack(fill=tk.BOTH, side=tk.LEFT, expand=True) # frame set to expand and be responsive no matter window size
 
