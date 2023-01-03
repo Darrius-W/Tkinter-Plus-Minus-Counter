6ee6564f130922772b3e6db2bd8a821a4c040209
Darrius-W <you@example.com>
2023-01-03 16:04:29 -0600

    Added minud button

--- plusMinusGUI.py
+++ plusMinusGUI.py
@@ -4,7 +4,8 @@ window = tk.Tk() # creates instance of tkinter class
 
 frame1 = tk.Frame(master=window, width=100, height=100, bg='blue') # frame for the whole application
 
-
+minus_btn = tk.Button(master=frame1, text='-', background='red')
+minus_btn.pack()
 
 frame1.pack(fill=tk.BOTH, side=tk.LEFT, expand=True) # frame set to expand and be responsive no matter window size
 
