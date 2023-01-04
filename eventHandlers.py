8bca719fcd4c2d763ebbfd599e0c60d56ea7af8d
Darrius-W <you@example.com>
2023-01-03 18:09:32 -0600

    Added increment functionality

---
+++ eventHandlers.py
@@ -0,0 +1,5 @@
+import plusMinusGUI as PMGUI
+def increase_res():
+    result = PMGUI.display_lbl['text']
+    result = int(PMGUI.display_lbl['text']) + 1
+    PMGUI.display_lbl['text'] = result
--- plusMinusGUI.py
+++ plusMinusGUI.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import eventHandlers as EH
 
 window = tk.Tk() # creates instance of tkinter class
 
@@ -9,11 +10,11 @@ minus_btn = tk.Button(master=frame1, text='-', bg='red')
 minus_btn.pack(side=tk.LEFT)
 
 # Result display
-display_lbl = tk.Label(master=frame1, text='"display num"', bg='yellow')
+display_lbl = tk.Label(master=frame1, text='0', bg='yellow')
 display_lbl.pack(side=tk.LEFT)
 
 # Add 1 Button
-add_btn = tk.Button(master=frame1, text='+', bg='green')
+add_btn = tk.Button(master=frame1, text='+', bg='green', command=EH.increase_res)
 add_btn.pack(side=tk.LEFT)
 
 frame1.pack(fill=tk.BOTH, side=tk.LEFT, expand=True) # frame set to expand and be responsive no matter window size
