cd206d47606a782103bbe5f05926c95adfa10844
Darrius-W <you@example.com>
2023-01-03 19:37:29 -0600

    Adjusted window size to encompass window title

--- plusMinusGUI.py
+++ plusMinusGUI.py
@@ -27,15 +27,15 @@ window.title('Plus-Minus Counter')
 frame1 = tk.Frame(master=window, bg='black') # frame for the whole application
 
 # Minus 1 Button
-minus_btn = tk.Button(master=frame1, text='-', bg='red', width=10, height=5, command=decrease_res, relief=tk.RAISED)
+minus_btn = tk.Button(master=frame1, text='-', bg='red', width=12, height=5, command=decrease_res, relief=tk.RAISED)
 minus_btn.pack(fill=tk.Y, side=tk.LEFT)
 
 # Result display
-display_lbl = tk.Label(master=frame1, text='0', width=10, height=5, bg='yellow', relief=tk.FLAT)
+display_lbl = tk.Label(master=frame1, text='0', width=12, height=5, bg='yellow', relief=tk.FLAT)
 display_lbl.pack(fill=tk.Y, side=tk.LEFT)
 
 # Add 1 Button
-add_btn = tk.Button(master=frame1, text='+', bg='green', width=10, height=5, command=increase_res, relief=tk.RAISED)
+add_btn = tk.Button(master=frame1, text='+', bg='green', width=12, height=5, command=increase_res, relief=tk.RAISED)
 add_btn.pack(fill=tk.Y, side=tk.LEFT)
 
 frame1.pack(fill=tk.BOTH, side=tk.LEFT, expand=True) # frame set to expand and be responsive no matter window size
