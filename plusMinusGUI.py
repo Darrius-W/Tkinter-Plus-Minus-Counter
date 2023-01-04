cc903152fac6592619c64c7def316f2e5617db9a
Darrius-W <you@example.com>
2023-01-03 18:48:17 -0600

    Quick Fix

--- plusMinusGUI.py
+++ plusMinusGUI.py
@@ -1,12 +1,24 @@
 import tkinter as tk
-import eventHandlers as EH
+
+# EVENT HANDLERS
+def increase_res():
+    result = display_lbl['text']
+    result = int(display_lbl['text']) + 1
+    display_lbl['text'] = result
+    
+def decrease_res():
+    result = display_lbl['text']
+    result = int(display_lbl['text']) - 1
+    display_lbl['text'] = result
+    
+    
 
 window = tk.Tk() # creates instance of tkinter class
 
 frame1 = tk.Frame(master=window, width=100, height=100, bg='blue') # frame for the whole application
 
 # Minus 1 Button
-minus_btn = tk.Button(master=frame1, text='-', bg='red', command=EH.decrease_res)
+minus_btn = tk.Button(master=frame1, text='-', bg='red', command=decrease_res)
 minus_btn.pack(side=tk.LEFT)
 
 # Result display
@@ -14,7 +26,7 @@ display_lbl = tk.Label(master=frame1, text='0', bg='yellow')
 display_lbl.pack(side=tk.LEFT)
 
 # Add 1 Button
-add_btn = tk.Button(master=frame1, text='+', bg='green', command=EH.increase_res)
+add_btn = tk.Button(master=frame1, text='+', bg='green', command=increase_res)
 add_btn.pack(side=tk.LEFT)
 
 frame1.pack(fill=tk.BOTH, side=tk.LEFT, expand=True) # frame set to expand and be responsive no matter window size
